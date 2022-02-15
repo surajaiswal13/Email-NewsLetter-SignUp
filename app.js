1e01809008ba25ba4b629a75afcd8d88e88632e9
Suraj Jaiswal <surajaiswal13@gmail.com>
2022-02-16 02:11:41 +0530

    Added post functionaloty to form

--- app.js
+++ app.js
@@ -5,10 +5,21 @@ const request = require("request");
 const app = express();
 
 app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended:true}))
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/signup.html")
 });
+
+app.post("/", (req, res) => {
+  const fname = req.body.fname
+  const lname = req.body.lname
+  const email = req.body.email
+
+  console.log(fname);
+  console.log(lname);
+  console.log(email);
+})
 
 app.listen(3000, () => {
   console.log("Server is Listening to port 3000");
