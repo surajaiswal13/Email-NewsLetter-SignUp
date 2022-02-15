512d6e801948f2ec8ddccf3a3c7928add8ba7c56
Suraj Jaiswal <surajaiswal13@gmail.com>
2022-02-16 02:02:32 +0530

    Serving Static Files

--- app.js
+++ app.js
@@ -4,6 +4,12 @@ const request = require("request");
 
 const app = express();
 
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/signup.html")
+});
+
 app.listen(3000, () => {
   console.log("Server is Listening to port 3000");
 })
